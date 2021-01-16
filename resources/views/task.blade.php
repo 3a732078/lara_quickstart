5c4a5c4b6ed8df8e92db6f876ac1cfd1f9b8360f
Ben Peng <3a732078@gm.student.ncut.edu.tw>
2021-01-16 08:51:14 +0800

    寫入目前任務的列表，以及新增刪除按鈕

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -32,5 +32,27 @@
         </form>
     </div>
 
-    <!-- 代辦：目前任務 -->
+    <!-- 目前任務 -->
+    @if (count($tasks) > 0)
+        <tbody>
+        @foreach ($tasks as $task)
+            <tr>
+                <!-- 任務名稱 -->
+                <td class="table-text">
+                    <div>{{ $task->name }} </div>
+                </td>
+                <td>
+                    <!-- 刪除按鈕 -->
+                    <td>
+                        <form action="/task/{{ $task->id }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button>刪除任務</button>
+                        </form>
+                    </td>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    @endif
 @endsection
